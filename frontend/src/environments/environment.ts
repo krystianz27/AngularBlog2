1a1bd47cfdd6cc75bef8f69d512f3050ca10ce83
Krystian <226311@student.uek.krakow.pl>
2024-10-29 23:05:59 +0100

    environments.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  BACKEND_API_URL: 'http://localhost:3000',
+  // BACKEND_API_URL: 'http://localhost:3000',
+  BACKEND_API_URL:
+    'https://angularblog2-backend-654170692236.europe-central2.run.app',
 };
