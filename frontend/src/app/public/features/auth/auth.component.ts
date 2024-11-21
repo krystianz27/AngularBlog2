a549a1660fe438d58a12194b6cbc645ac9009125
Krystian <226311@student.uek.krakow.pl>
2024-11-21 15:23:59 +0100

    Auth token

--- frontend/src/app/public/features/auth/auth.component.ts
+++ frontend/src/app/public/features/auth/auth.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../core/components/header/header.component';
+import { FooterComponent } from '../../core/components/footer/footer.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
